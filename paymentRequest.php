9a9aa44d7a1a3ff2322ff19fc419ead36a01decc
marklaz1 <66415625+marklaz1@users.noreply.github.com>
2020-11-24 11:59:47 +0200

    Add files via upload

--- paymentRequest.php
+++ paymentRequest.php
@@ -11,6 +11,7 @@
 
 <h2>African Payment Solutions : Mastercard Payment Gateway - Hosted Payment Page request template</h2>
 <h3><a href="https://github.com/marklaz1/APS/blob/main/paymentRequest.php">See .PHP code here</a></h3>
+<h3><a href="https://ap-gateway.mastercard.com/api/documentation/integrationGuidelines/hostedCheckout/integrationModelHostedCheckout.html?locale=en_US">MPGS Hosted Checkout Integration spec</a></h3>
 <form action="paymentRequest.php" method="post">
 OrderId: <input type="text" name="orderId" value="orderno1"><br>
 Amount: <input type="text" name="amount" value="123.45"><br>
